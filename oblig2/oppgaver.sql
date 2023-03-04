2e259fee4a7bdffe82721e6bb0e5e6e94636a244
kegma1 <kennetam02@gmai.com>
2023-03-04 15:32:19 +0100

    ting og tang

--- oblig2/oppgaver.sql
+++ oblig2/oppgaver.sql
@@ -1,5 +1,9 @@
 #oppgave 1
-
+select o.OrderNR, o.LeveringsDato, o.BestillingsDato, m.MåltidNavn, a.AvdelingsNavn
+from måltidsordrer as o 
+inner join måltider as m on o.Måltid = m.MåltidNR
+inner join avdelinger as a on o.Mottaker = a.AvdelingNR
+where status = "ikke behandlet";
 #oppgave 2
 INSERT INTO `stud_v23_kma150`.`måltidsordrer` (`Måltid`, `Status`, `Mottaker`, `LeveringsDato`) VALUES ('2', 'ikke behandlet', '2', '2023-03-04');
 INSERT INTO `stud_v23_kma150`.`måltidsordrer` (`Måltid`, `Status`, `Mottaker`, `LeveringsDato`) VALUES ('2', 'ikke behandlet', '2', '2023-03-04');
@@ -11,3 +15,13 @@ INSERT INTO `stud_v23_kma150`.`måltidsordrer` (`Måltid`, `Status`, `Mottaker`,
 INSERT INTO `stud_v23_kma150`.`måltidsordrer` (`Måltid`, `Status`, `Mottaker`, `LeveringsDato`) VALUES ('3', 'ikke behandlet', '2', '2023-03-04');
 INSERT INTO `stud_v23_kma150`.`måltidsordrer` (`Måltid`, `Status`, `Mottaker`, `LeveringsDato`) VALUES ('4', 'ikke behandlet', '2', '2023-03-04');
 INSERT INTO `stud_v23_kma150`.`måltidsordrer` (`Måltid`, `Status`, `Mottaker`, `LeveringsDato`) VALUES ('4', 'ikke behandlet', '2', '2023-03-04');
+#oppgave 3
+select * from basismatvarer where Matvare in 
+(select basismatvarer_Matvare from basismatvarer_has_måltider where måltider_MåltidNR = 
+(select MåltidNR from måltider where MåltidNavn = "Spagetti carbonara"));
+#oppgave 4
+
+#oppgave 5
+
+#oppgave 6
+
