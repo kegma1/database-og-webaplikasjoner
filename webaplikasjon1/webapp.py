5912d1fedfa2df3a87cc32ea002385d1febfa56e
kegma1 <kennetam02@gmai.com>
2023-03-25 20:25:32 +0100

    did a thing

---
+++ webaplikasjon1/webapp.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Brainfuck")
+
+@app.route("/kontaktskjema", methods = ["POST", "GET"])
+def kontaktskjema():
+    if request.method == "POST":
+        return render_template("displaystuff.html",
+            titel="Display form",
+            name=request.form["Navn"],
+            email=request.form["Epostadresse"],
+            b_day=request.form["Fødselsdato"],
+            web_url=request.form["Webadresse"],
+            grade=request.form["Klassenavn"],
+            msg=request.form["Meldingsboks"]
+        )
+    else:
+        return render_template("kontaktskjema.html", title="Kontak skjema")
+
+if __name__ == "__main__":
+    app.run()
